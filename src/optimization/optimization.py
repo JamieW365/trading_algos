dfd50fa2cef84928922f734cd8971adef9535ad6
JamieW365 <jamiewelch1011@gmail.com>
2026-02-27 15:26:02 +0000

    calculate weights EWP v1

--- src/optimization/optimization.py
+++ src/optimization/optimization.py
@@ -16,3 +16,24 @@ def select_n(n: int=5,
         random.seed(seed)
 
     return random.sample(stocks, n)
+
+def calculate_weights(df_stocks,
+                      method: str='EWP'):
+
+    '''
+    Return weights matrix for a given portfolio of stocks
+    '''
+
+    # Run weighting calculations dependent on the selected methodology
+    match method:
+        # Equal Weighted Portfolio
+        case 'EWP':
+            # Each asset is given a weight of 1/N for N assets
+            num_assets = len(df_stocks.Close.columns)
+            weights = np.array([1] * num_assets) / num_assets
+
+        # ValueError if an invalid methodology is into the function
+        case _:
+            raise ValueError(f'{method} an invalid weighting method, input a pre-defined weighting methodology.')
+
+    return weights
