eac27cf719c9cf3ce73f7d4810df2f5a65be4fff
JamieW365 <jamiewelch1011@gmail.com>
2026-03-16 18:10:49 +0000

    load data from a saved file

--- trading_algos/datasets.py
+++ trading_algos/datasets.py
@@ -4,12 +4,18 @@ import numpy as np
 # import random
 import warnings
 
-from trading_algos.config import EXTERNAL_DATA_DIR
+from trading_algos import config as tac
 
-def get_sp500_meta(get_latest: bool=False):
+def get_sp500_meta(get_latest:       bool = False,
+                   meta_table:       str  = 'full',
+                   curr_path:        str  = tac.EXTERNAL_DATA_DIR,
+                   curr_filename:    str  = 'sp500_current.csv',
+                   changes_path:     str  = tac.EXTERNAL_DATA_DIR,
+                   changes_filename: str  = 'sp500_changes.csv'):
 
     '''
-    Returns the current S&P 500 metadata from Wikipedia detailing the following information for each security:
+    Returns the current S&P 500 metadata from Wikipedia detailing the
+    following information for each security:
 
         Symbol (ticker)
         Security (full name)
@@ -21,64 +27,123 @@ def get_sp500_meta(get_latest: bool=False):
         Founded (year founded)
     
     Note: 
-        This is not the most ideal way to retrieve this information. New attempts to read the data via URL
-        may fail for many reasons, including: server side issues, blocks to API calls, website changes etc.
-        However, without utilising a paid-for API, it will provide enough functionality to serve it's purpose.
+        This is not the most ideal way to retrieve this information.
+        New attempts to read the data via URL may fail for many reasons,
+        including: server side issues, blocks to API calls, website
+        changes etc. However, without utilising a paid-for API, it will
+        provide enough functionality to serve it's purpose.
 
     Parameters:
         get_latest (default = False)
-            When True this will make a URL call to Wikipedia to retrieve the most up to date information available.
-            When False this will load the latest available information from a saved data source.
+            When True this will make a URL call to Wikipedia to retrieve
+                the most up to date information available.
+            When False this will load the latest available information
+                from a saved data source.
     '''
     
-    # Make a new call to Wikipedia to retrieve the latest S&P metadata
+    curr_filepath = curr_path/curr_filename
+    changes_filepath = changes_path/changes_filename
+
     if get_latest:
-        # Attempt to retrieve the latest S&P information from Wikipedia. There is always a chance that this
-        # will fail. Catch any error and fallback on loading saved data if necessary.
+        # Attempt to load the latest metadata from Wikipedia
+        # This may fail for a number of reasons, such as website change,
+        # server unavailability, etc.
         try:
             wiki = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies',
                                 storage_options={'User-Agent': 'pandas'},
                                 index_col=0)
-            df_current = wiki[0]
-            df_current.to_csv(EXTERNAL_DATA_DIR / 'sp500_components.csv')
-        # If pandas fails to read data from Wikipedia for any reason then revert to loading the latest available saved data
-        except Exception as err:
-            # Attempt to load the latest S&P information from the latest available saved data
-            try:
-                df_current = pd.read_csv(EXTERNAL_DATA_DIR / 'sp500_components.csv', index_col=0)
-            # If unable to load saved data then we must error at this point
-            except:
-                raise Exception(err)
-    # Attempt to load the latest S&P information from the latest available saved data
-    else:
-        # Attempt to load the latest S&P information from the latest available saved data
-        try:
-            df_current = pd.read_csv(EXTERNAL_DATA_DIR / 'sp500_components.csv', index_col=0)
-        # If unable to load saved data then we must error at this point
-        except:
-            raise Exception(err)
             
-    return df_current
+            # The first wiki table contains the current S&P 500 metadata
+            df_current_meta = wiki[0].copy()
+            df_current_meta.reset_index(inplace=True)
+            df_current_meta['Date added'] = pd.to_datetime(df_current_meta['Date added'])         
+            df_current_meta.set_index(df_current_meta['Date added'], inplace=True)
+            df_current_meta.sort_index(inplace=True)
+            df_current_meta.drop('Date added', axis=1, inplace=True)
+
+            # Save metadata locally so that wikipedia calls can be
+            # avoided as much as possible
+            df_current_meta.to_csv(curr_filepath)
+            
+            # The second wiki table contains an extensive list of stocks
+            # that were once included in the S&P500 but have been
+            # removed
+            df_changes_meta = wiki[1].copy()
+            df_changes_meta.index = pd.DatetimeIndex(df_changes_meta.index)
+            df_changes_meta.sort_index(inplace=True)
+            
+            # Save metadata locally so that wikipedia calls can be
+            # avoided as much as possible
+            df_changes_meta.to_csv(changes_filepath)
+
+        # If Wikipedia call fails then attempt to load the latest
+        # metadata from saved memory as backup
+        except Exception as err:    
+            raise Exception(err)
+              
+    try:
+        match meta_table:
+            case 'current':
+                return pd.read_csv(curr_filepath, index_col=0)
+            case 'past':
+                return pd.read_csv(changes_filepath, index_col=0, header=[0,1])
+            case _:
+                return pd.read_csv(curr_filepath, index_col=0), \
+                       pd.read_csv(changes_filepath, index_col=0, header=[0,1])
+                    
+    except Exception as err:
+        raise Exception(err)
+    
+def calc_sp500_survivors(get_latest: bool = True):
+
+    df_current, df_changes = get_sp500_meta(get_latest=get_latest)
+    df_changes_added = df_changes['Added'].copy()
+    df_changes_removed = df_changes['Removed'].copy()
+
+
+
+    return df_current, df_changes
+
+def get_sp500(get_latest: bool = False):
+
+    if get_latest:
+        return calc_sp500_survivors(get_latest=get_latest)
+
+    else:
+        try:
+            pass
+        except:
+            pass
+
+    pass
 
 def get_sp500_tickers(get_latest: bool=False):
 
     '''
-    Returns the up to date list of stocks currently in the S&P 500 from Wikipedia
+    Returns the up to date list of stocks currently in the S&P 500 from
+    Wikipedia
     '''
 
-    return get_sp500_meta(get_latest = get_latest).index.tolist()
+    return get_sp500_meta(get_latest=get_latest, meta_table='current').index.tolist()
 
-def load_data(tickers,
+def load_data(tickers: list=None,
+              file: str=None,
+              filepath: str=tac.RAW_DATA_DIR,
               start_date: None = None,
               end_date: None = None,
               columns: list = ['Close']):
     
     '''
-    This function downloads stock data from yfinance in a way that is consistent for use throughout the trading_algos
-    repository
+    This function downloads stock data from yfinance in a way that is
+    consistent for use throughout the trading_algos repository
     '''
 
-    df_stocks = yf.download(tickers, start=start_date, end=end_date)[columns]
-
-
+    if file != None:
+        df_stocks = pd.read_csv(filepath/file,
+                                header=[0,1],
+                                index_col=0)
+        df_stocks.index = pd.to_datetime(df_stocks.index)
+    else:
+        df_stocks = yf.download(tickers, start=start_date, end=end_date)[columns]
+        
     return df_stocks
